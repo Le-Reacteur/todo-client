8c7afc8b1907bd68e7d6df95045a9ea871d82295
Farid Safi <faridsafi@noreply.github.com>
2018-11-25 19:02:58 +0100

    Create tasks form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./App.css";
 
+const domain = "http://localhost:3100";
+
 class App extends Component {
+  state = {
+    title: ""
+  };
+
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div>
+        <form
+          onSubmit={event => {
+            event.preventDefault();
+
+            axios
+              .post(`${domain}/create`, {
+                title: this.state.title
+              })
+              .then(response => {
+                if (response.status === 200) {
+                  console.log("response", response.data);
+                } else {
+                  console.error("An error occurred");
+                }
+              })
+              .catch(err => {
+                console.error(err);
+              });
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Title"
+            value={this.state.title}
+            onChange={event => {
+              this.setState({ title: event.target.value });
+            }}
+          />
+          <input type="submit" />
+        </form>
+      </div>
+    );
   }
 }
 
